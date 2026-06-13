27e988ff8b0d633956e0004fe3842287faf15e13
cortiz-maker <cortiz@quantrex.cl>
2026-06-12 21:32:48 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,10 +8,10 @@
 // ============================================================
 
 export const SUPABASE_URL =
-  import.meta.env.VITE_SUPABASE_URL || import.meta.env.VITE_SUPABASE_URL || https://yzjnmnzbkykkqhefqekp.supabase.co/rest/v1/;;
+  import.meta.env.VITE_SUPABASE_URL || "https://yzjnmnzbkykkqhefqekp.supabase.co";
 
 export const SUPABASE_ANON_KEY =
-  import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY || eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6am5tbnpia3lra3FoZWZxZWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMDY0MzAsImV4cCI6MjA5Njg4MjQzMH0.OXCwLwpR2dra4QeQaMmnJ9yhtG9YHPpCP05T4TdWVtc;;
+  import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6am5tbnpia3lra3FoZWZxZWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMDY0MzAsImV4cCI6MjA5Njg4MjQzMH0.OXCwLwpR2dra4QeQaMmnJ9yhtG9YHPpCP05T4TdWVtc";;
 
 export const PUENTE_URL =
   import.meta.env.VITE_PUENTE_URL ||
